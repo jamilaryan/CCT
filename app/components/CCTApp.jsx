4361597614cb9b4c410ef8ddf0b47c5bacb57539
Jamil Aryan <jamil@focus21.io>
2016-07-26 23:22:27 -0400

    adding some HTML tags

---
+++ app/components/CCTApp.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CCTApp = React.createClass({
+  render: () => {
+    return (
+      <div>
+        <h1>CCTApp: A database driven realtime webapp for a media server</h1>
+        <p>Home Page</p>
+      </div>
+    );
+  }
+});
+
+module.exports = CCTApp;
